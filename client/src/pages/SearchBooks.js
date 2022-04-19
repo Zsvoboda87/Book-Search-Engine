d57462308737eeb23b9910c9d288809acc0ffb98
Zsvob <svobodazachary@gmail.com>
2022-04-19 14:49:21 -0400

    fixes

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -18,7 +18,7 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
-  const [saveBook] = useMutation(SAVE_BOOK)
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
 
@@ -73,7 +73,7 @@ const SearchBooks = () => {
     try {
       const response = await saveBook({
         variables: {
-          input: bookToSave,
+          book: bookToSave,
         },
       });
       if (!response) {
